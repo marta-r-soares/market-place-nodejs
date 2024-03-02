29d976e33873ee92e50b6f452b06ef21d2ac9eb3
Marta-r-soares <marta.soaresadm@gmail.com>
2024-03-02 16:29:29 -0300

    Feat CRUD user service

---
+++ src/service/usuario.service.js
@@ -0,0 +1,51 @@
+const Usuario = require("../model/Usuario");
+
+const findUserByIdSerevice =  (id) => {
+    return Usuario.findById(id);
+}
+
+const findAllUsersService  =  () => {
+    return Usuario.find();
+}
+
+
+const createUserService  =  (body) => {
+    return Usuario.create(body);
+}
+
+const updateUserService  =  (id, body) => {
+    return Usuario.findByIdAndUpdate(id, body, {returnDocument: "after"});
+}
+
+
+const removeUserService  =  (id) => {
+    return Usuario.findByIdAndRemove(id);
+}
+
+const addUserAddressService  =  (id, endereco) => {
+
+}
+
+const removeUserAddressService  =  (id) => {
+
+}
+
+const addUserFavProductService  =  (id, produto) => {
+
+}
+
+const removeUserFavProductService  =  (produto) => {
+
+}
+
+module.exports = {
+    findUserByIdSerevice,
+    findAllUsersService,
+    createUserService,
+    updateUserService, 
+    removeUserService,
+    addUserAddressService,
+    removeUserAddressService,
+    addUserFavProductService,
+    removeUserFavProductService
+}
